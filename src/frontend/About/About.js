0fdd2a7f644c5262a1b345b0785f29060f35ca8d
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-29 14:25:36 +0000

    hi there section added

--- src/frontend/About/About.js
+++ src/frontend/About/About.js
@@ -5,21 +5,22 @@ export default function About() {
     return (
         <div className="container-fluid section" id="aboutSection">
             <h3>Hi There!</h3>
-            <div id="aboutDescription">
+                        <div id="aboutDescription">
+                        <p>
+                
+                I'm a fast and curious coder who is passionate about expanding my knowledge and skill base.
+                For front-end development I work with HTML5, CSS3 (Bootstrap), Javascript (React and Jquery). For back-end, I use Node.Js and Express.Js. I also work with MySQL and MongoDB for databases and am experienced with various testing frameworks and APIs.
+               
+        </p>
+                <h3>My Story</h3>
                 <p>
-                    With a background in Anthropology and International Relations, I followed my curiosity to Japan, where I lived, travelled and worked for five years. I went from teaching high school English in a remote fishing town to the communications department at the School of Science of Japan's most esteemed university in central Tokyo.
+                    Having grown-up in London and with a background in Anthropology and International Relations, I followed my curiosity for other cultures to Japan, where I lived, travelled and worked for five years. I went from teaching high school English in a remote fishing town, to working in the School of Science communications department at The University of Tokyo.
 
-                    It was there I learnt the importance of hard skills, and worked alongside web developers to create various websites for the university, peaking my interest. One of the highlights was using Google Analytics on our site, and completing the Google Analytics Academy courses.
+                    It was there I learnt the importance of hard skills and worked alongside web developers to create various websites for the university, peaking my interest in tech. One of the highlights was using Google Analytics on our site, and completing the Google Analytics Academy courses.
                     <br /><br />
-                    Upon returning to the UK, I again working in a communications role alongside web developers and IT professionals. By this point my job-envy was undeniable - I needed to get a piece of the action and unravel the mysteries of thier work!
-                    <br />
-                    In early 2019, I decided to finally take the plunge. Start afresh and learn to code. Re-wiring the part of my brain that previously dedicated to studying Japanese and my desire peer over the wall of the inner workings of the tech-world, and oh boy, I've loved every second of it.
+                    Upon returning to the UK, I started working in a communications and marketing role alongside web developers and IT professionals. By this point my job-envy was undeniable - I needed to get a piece of the action and unravel the mysteries of their work! In early 2019, I decided to finally take the plunge: start afresh and learn to code. Re-wiring the part of my brain that I had previously dedicated to studying Japanese and my desire to peer over the wall of the inner workings of the tech-world, and oh boy, I've loved every second of it.
                     <br /><br />
-                    I completed the <a href='https://www.codecademy.com/learn/paths/web-development' target="_blank" rel="noopener noreferrer"> Codecademy full stack web development course</a> covering HTML5, CSS3, Javascript, Node, building and connecting to API's, SQL and TDD methods. This included various frameworks and libraries such as Handlebars, React, jQuery, Express, Chai and Mocha. In October 2019, I joined <a href='https://kodiri.com/bootcamp' target="_blank" rel="noopener noreferrer">Kodiri</a>, a two-month intensive coding bootcamp covering mobile-first UI and UX HTML5 and CSS3, Javascript, backend APIs, NodeJs and ExpressJs, MongoDB, cloud deployment with Heroku 
-
-                    I began training as a front and back end developer in April 2019. I use HTML 5, CSS (Bootstrap), Javascript (React, Jquery, Handlebars, Node, Express) for front-end development, Node and Express for back-end development and MySQL and MongoDB for databases.<br /><br />
-
-                    My previous experience is in communications and marketing. I was born and raised in London and spent 5 years living and working in Japan.
+                    I completed the <a className='aboutAnchor' href='https://www.codecademy.com/learn/paths/web-development' target="_blank" rel="noopener noreferrer"> Codecademy Web Development Career Path Course</a> covering HTML5, CSS3, Javascript, Node, building and connecting to API's, SQL and TDD methods. This included various frameworks and libraries such as Handlebars, React, jQuery, Express, Chai and Mocha. In October 2019, I joined <a className='aboutAnchor' href='https://kodiri.com/bootcamp' target="_blank" rel="noopener noreferrer">Kodiri</a> - a two-month intensive coding bootcamp covering mobile-first UI and UX design with HTML5 and CSS3, Javascript, backend APIs, NodeJs, ExpressJs, MongoDB and cloud deployment with Heroku and ZEIT Now.
             </p>
             </div>
         </div>
