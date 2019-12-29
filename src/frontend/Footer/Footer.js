cf0f7e0fa8f0c66fd233f92d5990013776f15e38
Zara <48896844+zarbar@users.noreply.github.com>
2019-12-29 17:23:41 +0000

    about description finished with emojis and logos

--- src/frontend/Footer/Footer.js
+++ src/frontend/Footer/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className="container-fluid">
             <div id="foot"><h5>Made with ♡ by Zara</h5></div>
-            <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <div>Icons made by <a className='footer-anchor' href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a className='footer-anchor' href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </footer>
     )
 }
