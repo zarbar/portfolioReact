8a911381770b771ac45c30f67af980890d83ce7e
Zara <48896844+zarbar@users.noreply.github.com>
2020-01-13 23:09:19 +0000

    added Where in the World to portfolio items

--- src/frontend/Portfolio/Portfolio.js
+++ src/frontend/Portfolio/Portfolio.js
@@ -8,7 +8,8 @@ export default function Portfolio() {
     const rickMorty = PortfolioItemList()[0];
     const festipal = PortfolioItemList()[1];
     const bananaQuiz = PortfolioItemList()[2];
-    const kodflix = PortfolioItemList()[3];
+    const whereWorld = PortfolioItemList()[3];
+    const kodflix = PortfolioItemList()[4];
     let projectsComingSoon = ['E-commerce website for jewellery designer Purple Zoe'];
 
     let renderProjectsComingSoon = projectsComingSoon.map((project) => {
@@ -32,6 +33,12 @@ export default function Portfolio() {
             />
             <PortfolioItemDisplay
                 item={bananaQuiz}
+                comingSoon={false}
+            />
+
+
+            <PortfolioItemDisplay
+                item={whereWorld}
                 comingSoon={false}
             />
 
